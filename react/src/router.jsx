69dba0f4c15934fb2d5cf9546409f85cc5980df3
auloy15 <nasidurrahman1606@gmail.com>
2025-02-07 21:33:20 +0600

    create post page

--- react/src/router.jsx
+++ react/src/router.jsx
@@ -1,21 +1,29 @@
-import { createBrowserRouter } from 'react-router-dom'
-import Register from './auth/register'
-import Home from './views/home'
-import Login from './auth/login'
-import Profile from './views/profile'
-import Layout from './views/Layout'
-import AuthCallback from './auth/google-callback.jsx'
+import { createBrowserRouter } from "react-router-dom";
+import Register from "./auth/register";
+import Home from "./views/home";
+import Login from "./auth/login";
+import Profile from "./views/profile";
+import Layout from "./views/Layout";
+import AuthCallback from "./auth/google-callback.jsx";
+import AskQuestion from "./views/AskQuestion.jsx"; 
+
+
+ 
+
 const router = createBrowserRouter([
   {
     element: <Layout />,
     children: [
-      { path: '/', element: <Home /> },
-      { path: '/register', element: <Register /> },
-      { path: '/login', element: <Login /> },
-      { path: '/profile', element: <Profile /> },
-      { path: "/auth/google-callback", element: <AuthCallback /> }
+      { path: "/", element: <Home /> },
+      { path: "/register", element: <Register /> },
+      { path: "/login", element: <Login /> },
+      { path: "/profile", element: <Profile /> },
+      { path: "/auth/google-callback", element: <AuthCallback /> },
+      { path: "/ask-question", element: <AskQuestion /> },
+     
+
     ],
   },
-])
+]);
 
-export default router
+export default router;
