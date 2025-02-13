d2d0183f81b0b387525f953856dbd853c34c7ee9
auloy15 <nasidurrahman1606@gmail.com>
2025-02-14 00:34:27 +0600

    added sign in page for admin

--- admin/src/router.jsx
+++ admin/src/router.jsx
@@ -2,12 +2,13 @@ import React from 'react';  // Add this line
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "./pages/Layout";
 import Home from "./pages/Home";
-
+import SignIn from './pages/auth/signIn';
 const router = createBrowserRouter([
     {
       element: <Layout />,
       children: [
         { path: "/", element: <Home /> },
+        {path: '/login', element: <SignIn/>},
         // { path: '/about', element: <About /> },
         // Protected routes
         // {
